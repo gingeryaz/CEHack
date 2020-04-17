6e1ea5dc15dea156f612a1e2623bd58d5c8d7a75
Harleen C <harleenc@bu.edu>
2020-04-17 16:03:16 -0400

    Added

---
+++ app/src/main/java/com/example/bytesizec/LecturesMenu.java
@@ -0,0 +1,38 @@
+package com.example.bytesizec;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LecturesMenu extends AppCompatActivity {
+    public Button pointers, inout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lectures_menu);
+
+        inout = (Button) findViewById(R.id.inoutbutton);
+        inout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LecturesMenu.this,FileInOut.class);
+                startActivity(intent);
+            }
+        });
+
+
+        pointers = (Button) findViewById(R.id.pointerbutton);
+        pointers.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LecturesMenu.this,VideoWatchPointers.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
